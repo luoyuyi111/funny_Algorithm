24c14b73a840c1ee3efff41b7d837befde2fa193
b654381241 <653481241@qq.com>
2019-09-05 14:34:10 +0800

    update1

---
+++ linklist/a.h
@@ -0,0 +1,17 @@
+#pragma once
+struct List{
+	int value;
+	List *next;
+	int length;
+	List *pre;
+}
+class LinkList{
+	LinkList();
+	~ LinkList();
+	void insertdata1(int u);
+	void insertdata2(int u);
+	void updatedata(int position,int u);
+	void deletedata(int position);
+	int lengthdata();
+	int selectdata(int position);
+}
